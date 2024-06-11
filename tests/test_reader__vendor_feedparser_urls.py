38767a8aa55da538d7061dab6c2ab2493dfd95b4
KubaDomagala <112829440+KubaDomagala@users.noreply.github.com>
2024-06-11 23:32:31 +0200

    coverage tool for url function

--- tests/test_reader__vendor_feedparser_urls.py
+++ tests/test_reader__vendor_feedparser_urls.py
@@ -1,8 +1,14 @@
-from reader._vendor.feedparser.urls import _urljoin
+from reader._vendor.feedparser.urls import branch_coverage
 from reader._vendor.feedparser.urls import make_safe_absolute_uri
+from reader._vendor.feedparser.urls import print_coverage_url
 
 
 def test_make_safe_absolute_uri():
     base = "scheme://netloc/path;params?query#fragment"
     assert make_safe_absolute_uri(base) == ""
     assert make_safe_absolute_uri(base, "scheme") == ""
+
+    print_coverage_url()
+
+
+test_make_safe_absolute_uri()
